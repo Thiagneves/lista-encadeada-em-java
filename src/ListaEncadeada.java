076a01d288a87bfcc825bccf4ad154afeb33e707
Thiago Neves <thiagonevestg0@gmail.com>
2025-08-28 19:54:36 -0300

    primeiro commit

---
+++ src/ListaEncadeada.java
@@ -0,0 +1,13 @@
+public class ListaEncadeada {
+    public class Lista<T>{
+        private No<T> inicio;
+
+        public void inserirNoInicio(T valor){
+            if (inicio == null){
+                No<T>{
+
+                }
+            }
+        }
+    }
+}
